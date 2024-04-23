a66d01faef343306e34ba967ad169d66e8d89975
Yovany Suárez Silva <yovanysuarezsilva@gmail.com>
2024-04-22 23:32:19 -0500

    feat: Updated readme file information, added documentation in swagger, and added a license file

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -2,13 +2,15 @@ import express from 'express';
 
 const router = express.Router();
 import { validateData } from '../middlewares/dataValidation';
-import { deleteCharacterById, getAllCharacters, getCharacterById, getSearhByCharacters, setCharacter, updateCharacter } from '../controllers/character';
+import { deleteCharacterById, getAllCharacters, getCharacterById, getCharacterByName, getSearhByCharacters, setCharacter, updateCharacter } from '../controllers/character';
 
-router.post('/character/search', validateData, getSearhByCharacters);
+
+router.post('/character/search', validateData, getSearhByCharacters); // graphql external
 router.get('/character/all', validateData, getAllCharacters);
-router.get('/character/:id', validateData, getCharacterById);
+router.get('/character/id/:id', validateData, getCharacterById);
+router.get('/character/name/:name', validateData, getCharacterByName);
 router.post('/character', validateData, setCharacter);
-router.put('/character', validateData, updateCharacter);
+router.put('/character/:id', validateData, updateCharacter);
 router.delete('/character/:id', validateData, deleteCharacterById);
 
 module.exports = router;
