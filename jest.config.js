b78a9165eea2816d0e70f6ef9fcfe9f7015a2ce4
Yovany Suárez Silva <yovanysuarezsilva@gmail.com>
2024-04-26 00:05:36 -0500

    feat: configure ESLint, git-pre-commit, update docs & format code

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-module.exports = {
-  moduleNameMapper: {
-    "^@src/(.*)$": "<rootDir>/src/$1",
-  },
-  testMatch: ["**/__tests__/**/*.{ts,tsx}", "**/?(*.)(spec|test).{ts,tsx}"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
+export const moduleNameMapper = {
+  "^@src/(.*)$": "<rootDir>/src/$1",
+};
+export const testMatch = [
+  "**/__tests__/**/*.{ts,tsx}",
+  "**/?(*.)(spec|test).{ts,tsx}",
+];
+export const transform = {
+  "^.+\\.(ts|tsx)$": "ts-jest",
 };
